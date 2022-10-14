179ec7a6d94ff57afa4875cbb3e2f7a4800a7449
Eduardo Hernandez <eehernandez@gmail.com>
2022-10-14 17:16:10 -0300

    Eliminado archivo de ruta de productos

--- src/routes/product.router.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { productController } from "../controllers/index.js";
-const router = Router();
-
-router.route("/").get(productController.getProductos).post(productController.addProducto);
-router.route("/:id").get(productController.getProductos).delete(productController.removeProducto);
-
-export default router;
